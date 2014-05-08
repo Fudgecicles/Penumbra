f0b36e178bf91cede231b9f52b53ab0b06df1124
Steven Li <listeven@usc.edu>
2014-05-07 19:05:56 -0700

    sounds and shit

--- Assets/Scripts/ExitToLevelScreen.cs
+++ Assets/Scripts/ExitToLevelScreen.cs
@@ -11,7 +11,7 @@ public class ExitToLevelScreen : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Escape)) {
-			Application.LoadLevel (2);
+			Application.LoadLevel (3);
 		}
 	}
 }
